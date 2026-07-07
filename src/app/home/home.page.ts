2671edd4d8897ae0139e576beeed7f33167b2702
jsalcanizTCN <jstiven@tecon.es>
2026-07-07 13:47:36 +0200

    fix: import ChangeDetectorRef in home.page.ts to fix compilation error

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, inject, ViewChild, OnInit, OnDestroy } from '@angular/core';
+import { Component, inject, ViewChild, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { PluginListenerHandle } from '@capacitor/core';
 import { IonContent, IonFooter, IonHeader, IonToolbar, AlertController, ActionSheetController } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
