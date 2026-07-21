5b3d26d090bc228e9ca088c3cc402ab342d05b1b
jsalcanizTCN <jstiven@tecon.es>
2026-07-21 14:28:33 +0200

    Fix missing OnDestroy import

--- src/app/components/interactive-letter/interactive-letter.component.ts
+++ src/app/components/interactive-letter/interactive-letter.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output, Input, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
