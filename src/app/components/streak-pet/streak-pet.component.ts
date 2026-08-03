18bf715baf827f4359b1545c4d39779792a1b7d7
jsalcanizTCN <jstiven@tecon.es>
2026-08-03 13:47:18 +0200

    Restore ping-pong animation logic and user's reordered frames

--- src/app/components/streak-pet/streak-pet.component.ts
+++ src/app/components/streak-pet/streak-pet.component.ts
@@ -1204,7 +1204,14 @@ export class StreakPetComponent implements OnChanges, OnDestroy, OnInit {
 
   startPngAnimation() {
     this.animInterval = setInterval(() => {
-      this.animFrame = this.animFrame >= 4 ? 1 : this.animFrame + 1;
+      this.animFrame += this.animDirection;
+      if (this.animFrame >= 4) {
+        this.animFrame = 4;
+        this.animDirection = -1;
+      } else if (this.animFrame <= 1) {
+        this.animFrame = 1;
+        this.animDirection = 1;
+      }
     }, 250); // 4 FPS animation
   }
 
