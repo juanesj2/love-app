614419de9304407b8435cb606e3e3cbe12dc944d
jsalcanizTCN <jstiven@tecon.es>
2026-08-03 14:06:06 +0200

    Add 5th frame, re-align, and update logic to ping-pong 1-5

--- src/app/components/streak-pet/streak-pet.component.ts
+++ src/app/components/streak-pet/streak-pet.component.ts
@@ -1205,8 +1205,8 @@ export class StreakPetComponent implements OnChanges, OnDestroy, OnInit {
   startPngAnimation() {
     this.animInterval = setInterval(() => {
       this.animFrame += this.animDirection;
-      if (this.animFrame >= 4) {
-        this.animFrame = 4;
+      if (this.animFrame >= 5) {
+        this.animFrame = 5;
         this.animDirection = -1;
       } else if (this.animFrame <= 1) {
         this.animFrame = 1;
