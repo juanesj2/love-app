7d8edb9cd02913a58e8d04fe6f5e4431ef1f8777
jsalcanizTCN <jstiven@tecon.es>
2026-08-03 13:26:56 +0200

    Fix final animation crop and restore speed

--- src/app/components/streak-pet/streak-pet.component.ts
+++ src/app/components/streak-pet/streak-pet.component.ts
@@ -1212,7 +1212,7 @@ export class StreakPetComponent implements OnChanges, OnDestroy, OnInit {
         this.animFrame = 1;
         this.animDirection = 1;
       }
-    }, 1000); // 1 FPS animation for debugging
+    }, 250); // 4 FPS animation
   }
 
   getPngSrc(base: string): string {
